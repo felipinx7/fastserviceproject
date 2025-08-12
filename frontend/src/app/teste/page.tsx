83cbea9d6f6b5ef8e280e81d5347331241077de2
Felipe Lima <flimalimafelipek@gmail.com>
2025-08-12 20:27:11 -0300

    feat: update in frontend

--- frontend/src/app/teste/page.tsx
+++ frontend/src/app/teste/page.tsx
@@ -45,7 +45,7 @@ export default function Teste() {
         ) : (
           frutasFiltradas.map((fruta) => (
             <li key={fruta.id}>
-              {fruta.nome} - {fruta.cor}
+              {fruta.nome} - {fruta.cor} - ID: {fruta.id}
             </li>
           ))
         )}
